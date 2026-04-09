19ebf01b82c59ffdacc0967abf83260c642d8e60
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2026-04-09 14:55:33 +0900

    Remove (로컬에서 작업해버림)

--- Study/Source/Study/Player/BaseState/PlayerBaseState.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PlayerBaseState.h"
-
-void UPlayerBaseState::Init(AMyPlayer* Player_, UPlayerFSMComponent* FSM_)
-{
-	this->Player = Player_;
-	this->FSM = FSM_;
-}
